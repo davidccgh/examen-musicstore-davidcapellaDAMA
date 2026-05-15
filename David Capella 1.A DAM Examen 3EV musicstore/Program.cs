4fb508ea383260aa9fc8d9b740b98ed51a5761e0
David Capella <dcc.gm.2007@gmail.com>
2026-05-15 10:00:25 +0200

    Ejercicio 3: conexión y consultas MySQL

--- David Capella 1.A DAM Examen 3EV musicstore/Program.cs
+++ David Capella 1.A DAM Examen 3EV musicstore/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace David_Capella_1.A_DAM_Examen_3EV_musicstore
 {
@@ -31,45 +33,94 @@ namespace David_Capella_1.A_DAM_Examen_3EV_musicstore
                 return $"{titulo} - {artista} ({anyo})";
             }
         }
-        
-        static void Main(string[] args)
+        public class GestorBD
         {
-            String ruta = "C:\\Users\\Alumno1\\albumes.txt";
+            private MySqlConnection conexion;
 
-            List<Album> albums = new List<Album>
+            public GestorBD()
+            {
+
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "localhost";
+                builder.UserID = "root";
+                builder.Password = "1234";
+                builder.Database = "musicstore";
+
+                conexion = new MySqlConnection(builder.ConnectionString);
+            }
+
+            public void InsertarAlbum(Album album)
+            {
+                string query = "INSERT INTO album (titulo, artista, anyo, disponible) VALUES (@t, @a, @y, @d)";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+
+                cmd.Parameters.AddWithValue("@t", album.GetTitulo());
+                cmd.Parameters.AddWithValue("@a", album.GetArtista());
+                cmd.Parameters.AddWithValue("@y", album.GetAnyo());
+                cmd.Parameters.AddWithValue("@d", album.IsDisponible());
+
+                conexion.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            public void ObtenerTodosLosAlbumes()
+            {
+                string query = "SELECT * FROM album";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                conexion.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                   List<Album> albums = new List<Album>();
+                    string titulo = reader.GetString("titulo");
+                    string artista = reader.GetString("artista");
+                    int anyo = reader.GetInt32("anyo");
+                    bool disponible = reader.GetBoolean("disponible");
+                    Album album = new Album(titulo, artista, anyo, disponible);
+                    albums.Add(album);
+                }
+            }
+
+
+            static void Main(string[] args)
+            {
+                String ruta = "C:\\Users\\Alumno1\\albumes.txt";
+
+                List<Album> albums = new List<Album>
             {
                 new Album("Album1", "Metallica", 2000, true),
                 new Album("Album2", "Paco", 2005, false),
                 new Album("Album3", "Pedro", 2010, true)
             };
-            foreach (var album in albums)
-            {
-                Console.WriteLine(album.ToString());   
-            }
-            foreach(var album in albums)
-            {
-                if(album.GetArtista().Contains("Metallica"))
+                foreach (var album in albums)
                 {
-                    Console.WriteLine($"El album {album.GetTitulo()} de {album.GetArtista()}");
+                    Console.WriteLine(album.ToString());
+                }
+                foreach (var album in albums)
+                {
+                    if (album.GetArtista().Contains("Metallica"))
+                    {
+                        Console.WriteLine($"El album {album.GetTitulo()} de {album.GetArtista()}");
+                    }
+                }
+
+                Console.WriteLine($"\nFecha de hoy: {DateTime.Now.ToShortDateString()}");
+            }
+
+            public static void GuardarAlbum(List<Album> lista, string ruta)
+            {
+                using (StreamWriter writer = new StreamWriter(ruta))
+                {
+                    foreach (var album in lista)
+                    {
+                        writer.WriteLine($"{album.GetTitulo()};{album.GetArtista()};{album.GetAnyo()};{album.IsDisponible()}");
+                    }
                 }
             }
 
-            Console.WriteLine($"\nFecha de hoy: {DateTime.Now.ToShortDateString()}");
-        } 
 
-        public static void GuardarAlbum(List<Album> lista, string ruta)
-        {
-            using (StreamWriter writer = new StreamWriter(ruta))
-            {
-                foreach (var album in lista)
-                {
-                    writer.WriteLine($"{album.GetTitulo()};{album.GetArtista()};{album.GetAnyo()};{album.IsDisponible()}");
-                }
-            }
+
+
         }
-
-
-
-
     }
 }
