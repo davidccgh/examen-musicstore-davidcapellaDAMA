9d9d21dfac85358c408ecc66ebb01259cf908299
David Capella <dcc.gm.2007@gmail.com>
2026-05-15 09:36:38 +0200

    Ejercicio 2: lista, busqueda y fichero

--- David Capella 1.A DAM Examen 3EV musicstore/Program.cs
+++ David Capella 1.A DAM Examen 3EV musicstore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,23 +32,44 @@ namespace David_Capella_1.A_DAM_Examen_3EV_musicstore
             }
         }
         
-
-
         static void Main(string[] args)
         {
+            String ruta = "C:\\Users\\Alumno1\\albumes.txt";
 
+            List<Album> albums = new List<Album>
+            {
+                new Album("Album1", "Metallica", 2000, true),
+                new Album("Album2", "Paco", 2005, false),
+                new Album("Album3", "Pedro", 2010, true)
+            };
+            foreach (var album in albums)
+            {
+                Console.WriteLine(album.ToString());   
+            }
+            foreach(var album in albums)
+            {
+                if(album.GetArtista().Contains("Metallica"))
+                {
+                    Console.WriteLine($"El album {album.GetTitulo()} de {album.GetArtista()}");
+                }
+            }
 
+            Console.WriteLine($"\nFecha de hoy: {DateTime.Now.ToShortDateString()}");
+        } 
 
-
-
-
-
-
-
-
-
-
-
+        public static void GuardarAlbum(List<Album> lista, string ruta)
+        {
+            using (StreamWriter writer = new StreamWriter(ruta))
+            {
+                foreach (var album in lista)
+                {
+                    writer.WriteLine($"{album.GetTitulo()};{album.GetArtista()};{album.GetAnyo()};{album.IsDisponible()}");
+                }
+            }
         }
+
+
+
+
     }
 }
