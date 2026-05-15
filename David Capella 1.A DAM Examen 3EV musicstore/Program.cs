46b60f678cbc48a55b2320ad4ce7f20f9d9c2d01
David Capella <dcc.gm.2007@gmail.com>
2026-05-15 09:20:20 +0200

    Ejercicio 1: Clase Album Implementada

--- David Capella 1.A DAM Examen 3EV musicstore/Program.cs
+++ David Capella 1.A DAM Examen 3EV musicstore/Program.cs
@@ -8,6 +8,31 @@ namespace David_Capella_1.A_DAM_Examen_3EV_musicstore
 {
     internal class Program
     {
+        public class Album
+        {
+            private string titulo;
+            private string artista;
+            private int anyo;
+            private bool disponible;
+            public Album(string titulo, string artista, int anyo, bool disponible)
+            {
+                this.titulo = titulo;
+                this.artista = artista;
+                this.anyo = anyo;
+                this.disponible = disponible;
+            }
+            public string GetTitulo() => titulo;
+            public string GetArtista() => artista;
+            public int GetAnyo() => anyo;
+            public bool IsDisponible() => disponible;
+            public override string ToString()
+            {
+                return $"{titulo} - {artista} ({anyo})";
+            }
+        }
+        
+
+
         static void Main(string[] args)
         {
 
---
+++ David Capella 1.A DAM Examen 3EV musicstore/TextTemplate1.cs
@@ -0,0 +1,12 @@
+﻿a)  El símbolo - aparece delante de los atributos y el + delante de los métodos. ¿Qué significa cada 
+uno en términos de visibilidad? 
+
+El simbolo del guión representa que es privado y el + que es publico, el privado solo puede ser accedido dentro de la clase y el publico puede ser accedido desde cualquier parte del programa.
+
+b)  ¿Qué tipo de dato es disponible y que dos valores puede tener? 
+
+Disponible es de dato booleano y puede tener dos valores, true o false.
+
+c)  Para qué sirve el método ToString() en una clase de C#?
+
+Sirve para convertir los datos de un objeto a una cadena de texto string.
